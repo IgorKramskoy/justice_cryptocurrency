06fdac732ee0af590857764edef895346fc55d8c
Igor Kramskoy <ig.kramskoy@it-justice.com>
2022-06-30 17:55:39 +0300

    Edit custom alert

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,10 +2,14 @@ import { CustomButton } from './Common/CustomButton';
 import { CustomChip } from './Common/CustomChip';
 import { CustomTextField } from './Common/CustomTextField';
 import React from 'react';
+import { CustomAlert } from './Common/CustomAlert';
+import { Box } from '@mui/material';
 
 export const Test = () => {
   return (
-    <>
+    <Box sx={
+      { background:'#111823'}
+    }>
       <div>
         <CustomButton size="large" variant="contained" text="Button" disabled={false} color="primary"/>
         <CustomButton size="medium" variant="contained" text="Button" disabled={false} color="success"/>
@@ -19,7 +23,10 @@ export const Test = () => {
       </div>
 
       <CustomTextField id="outlined-basic" label="Имя" variant="outlined" />
-
-    </>
+      <CustomAlert text="text" color="success"/>
+      <CustomAlert text="text" color="error"/>
+      <CustomAlert isSmall text="text" color="success"/>
+      <CustomAlert isSmall text="text" color="error"/>
+    </Box>
   )
 }
