08bc1e2a64fec891db6716596b6d98684c532dea
Igor Kramskoy <ig.kramskoy@it-justice.com>
2022-07-04 17:30:30 +0300

    added market header.

--- src/components/Pages/Market/Market.jsx
+++ src/components/Pages/Market/Market.jsx
@@ -6,19 +6,10 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { TableCellHeadStyled, TableCellStyled } from './Table.styled';
-import { TextField } from '@mui/material';
+import { BoxStyled, TableCellHeadStyled, TableCellStyled } from './Table.styled';
+import { TextField, Typography } from '@mui/material';
 import { AutocompleteStyled } from './CustomAutocomplete.styled';
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-];
 const top100Films = [
   { label: 'The Shawshank Redemption', year: 1994, },
   { label: 'The Godfather', year: 1972 },
@@ -34,29 +25,34 @@ export const Market = () => {
   console.log(filteredRows)
   return (
     <>
-      <AutocompleteStyled
-        disablePortal
-        id="combo-box-demo"
-        options={top100Films}
-        renderInput={(params) => <TextField {...params} onChange={handleSearch} label="Поиск валюты" />}
-      />
-      <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650, }} aria-label="caption table">
-        <TableHead sx={{ background: '#191F29', padding: '0px'}}>
-          <TableRow  sx={{ }}>
-            <TableCellHeadStyled align="right">Calories</TableCellHeadStyled>
-            <TableCellHeadStyled align="right">Fat&nbsp;(g)</TableCellHeadStyled>
-          </TableRow>
-        </TableHead>
-        <TableBody sx={{ background: '#111823',}}>
-          {filteredRows.map((row) => (
-            <TableRow key={row.label}>
-              <TableCellStyled align="right">{row.label}</TableCellStyled>
-              <TableCellStyled align="right">{row.year}</TableCellStyled>
+      <BoxStyled>
+        <Typography variant="h4" sx={{ color: 'white' }}>
+           Курсы валют
+        </Typography>
+        <AutocompleteStyled
+          disablePortal
+          id="combo-box-demo"
+          options={top100Films}
+          renderInput={(params) => <TextField {...params} onChange={handleSearch} label="Поиск валюты" />}
+        />
+      </BoxStyled>
+      <TableContainer component={Paper} sx={{  borderRadius: '0px'}}>
+        <Table sx={{ minWidth: 650, borderRadius: '0px'}} aria-label="caption table">
+          <TableHead sx={{ background: '#191F29', padding: '0px'}}>
+            <TableRow  sx={{ }}>
+              <TableCellHeadStyled align="right">Calories</TableCellHeadStyled>
+              <TableCellHeadStyled align="right">Fat&nbsp;(g)</TableCellHeadStyled>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody sx={{ background: '#111823',}}>
+            {filteredRows.map((row) => (
+              <TableRow key={row.label}>
+                <TableCellStyled align="right">{row.label}</TableCellStyled>
+                <TableCellStyled align="right">{row.year}</TableCellStyled>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
     </TableContainer>
     </>
 
--- src/components/Pages/Market/Table.styled.js
+++ src/components/Pages/Market/Table.styled.js
@@ -1,4 +1,4 @@
-import { styled } from '@mui/material';
+import { Box, styled } from '@mui/material';
 import TableCell from '@mui/material/TableCell';
 
 export const TableCellHeadStyled = styled(TableCell) (() => ({
@@ -15,5 +15,12 @@ export const TableCellStyled = styled(TableCell) (() => ({
     fontSize: '14px',
     padding: '20px 32px',
     borderBottom:'1px solid rgba(255, 255, 255, 0.1)'
+  },
+}))
+export const BoxStyled = styled(Box) (() => ({
+  '&': {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
 }))
