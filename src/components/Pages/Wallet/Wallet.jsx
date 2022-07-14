7c609f3d3eec9dc45739e51d81f682d26f168c10
Igor Kramskoy <ig.kramskoy@it-justice.com>
2022-07-14 17:57:23 +0300

    added crypto table wallet.

--- src/components/Pages/Wallet/Wallet.jsx
+++ src/components/Pages/Wallet/Wallet.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import {
   Box,
   Button,
+  Pagination,
+  Paper,
   Table,
   TableBody,
+  TableContainer,
   TableHead,
   TableRow,
   Toolbar,
@@ -17,14 +21,18 @@ import { TableCellStyled } from './Wallet.styles';
 import rub from '../../../assets/images/rub.svg';
 import usd from '../../../assets/images/usd.svg';
 import * as Navigate from '../../../routesNavigate';
-import { useSelector } from 'react-redux';
 
 
 export const Wallet = () => {
   const navigate = useNavigate();
 
+  const [filteredRows, setFilteredRows] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(8);
+
   const walletUserRedux = useSelector((state) => state.money.walletUser);
-  const allWalletRedux = useSelector((state) => state.money.allWallets);
+  // const allWalletRedux = useSelector((state) => state.money.allWallets);
+  const currencies  = useSelector((state) => state.money.money );
 
   let rows = [
     {
@@ -47,14 +55,14 @@ export const Wallet = () => {
         img: rub,
         name: 'RUB',
         title: 'Russian Ruble',
-        cunt: walletUserRedux.currencies.rub ,
+        cunt: walletUserRedux.currencies.rub,
 
       },
       {
         img: usd,
         name: 'USD',
         title: 'American dollar',
-        cunt: walletUserRedux.currencies.usd ,
+        cunt: walletUserRedux.currencies.usd,
       },
     ]
   }
@@ -62,7 +70,42 @@ export const Wallet = () => {
   const currencyWithdrawal = () => {
     navigate(Navigate.REFILL)
   }
+  // const onChange = (event, newValue) => {
+  //   console.log(event, newValue);
+  //   if(!newValue){
+  //     setFilteredRows(currencies)
+  //   } else {
+  //     setFilteredRows(currencies.filter((item) => (
+  //       item.currency.includes(newValue.currency)
+  //     )))
+  //
+  //   }
+  // }
+  //
+  // const handleSearch = (e) => {
+  //   if(!e.target.value){
+  //     setFilteredRows(currencies)
+  //   } else {
+  //     setFilteredRows(currencies.filter((item) => (
+  //       item.currency.toLowerCase().includes(e.target.value.toLowerCase())
+  //     )))
+  //   }
+  // }
 
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage - 1);
+  };
+
+  // const handleChangeRowsPerPage = (event) => {
+  //   setRowsPerPage(parseInt(event.target.value, 10));
+  //   setPage(0);
+  // };
+
+  useEffect( () => {
+    if (filteredRows.length === 0) {
+      setFilteredRows(currencies)
+    }
+  }, [currencies]);
   return (
     <>
       <Toolbar
@@ -91,6 +134,7 @@ export const Wallet = () => {
           </Button>
         </Box>
       </Toolbar>
+      <Typography sx={{color: '#8C939D',textAlign:'left', marginTop:'30px', fontSize:'15px'}} variant="h5">Фиатый баланс</Typography>
       <BoxTableStyles sx={{marginTop: '20px'}}>
         <Table sx={{ minWidth: 650, borderRadius: '0px'}} aria-label="caption table">
           <TableHead sx={{ background: '#191F29', padding: '0px'}}>
@@ -126,6 +170,57 @@ export const Wallet = () => {
           </TableBody>
         </Table>
       </BoxTableStyles>
+      <Typography sx={{color: '#8C939D',textAlign:'left', marginTop:'30px', fontSize:'15px'}} variant="h5">Криптовалютный баланс</Typography>
+      <TableContainer component={Paper} sx={{
+        marginTop: '20px',
+        borderRadius: '0px',
+        boxShadow: 'none',
+        background: 'none',
+        marginBottom: '20px',
+      }}>
+        <Table sx={{ minWidth: 650, borderRadius: '0px'}} aria-label="caption table">
+          <TableHead sx={{ background: '#191F29', padding: '0px'}}>
+            <TableRow  sx={{ }}>
+              <TableCellHeadStyled align="left">Название</TableCellHeadStyled>
+              <TableCellHeadStyled align="left">Всего</TableCellHeadStyled>
+              <TableCellHeadStyled align="left">В ордере</TableCellHeadStyled>
+              <TableCellHeadStyled align="left">Доступно</TableCellHeadStyled>
+              <TableCellHeadStyled align="left">В ордере</TableCellHeadStyled>
+              <TableCellHeadStyled align="left"></TableCellHeadStyled>
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ background: '#111823',}}>
+            { filteredRows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => (
+                <TableRow key={`${row.name}-${row.currency}`}>
+                  <TableCellStyled align="left">
+                    <Box sx={{display: 'flex', alignItems: 'center', gap: '10px'}}>
+                      <Box><img src={row?.img} alt='icon'/></Box>
+                      <Box>{row?.currency}</Box>
+                      <Box>{row?.name}</Box>
+                    </Box>
+                  </TableCellStyled>
+                  <TableCellStyled align="left"></TableCellStyled>
+                  <TableCellStyled align="left"></TableCellStyled>
+                  <TableCellStyled align="left"></TableCellStyled>
+                  <TableCellStyled align="left"></TableCellStyled>
+                  <TableCellStyled align="left">
+                    <Button size="small" variant="contained" disabled={false} color="info">Торговать</Button>
+                  </TableCellStyled>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+        <Pagination
+          count={Math.ceil(filteredRows.length / rowsPerPage)}
+          shape="rounded"
+          rowsperpage={rowsPerPage}
+          page={page + 1}
+          onChange={handleChangePage}
+          // onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </TableContainer>
     </>
   );
 }
