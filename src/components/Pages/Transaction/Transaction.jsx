ed967c315d5835d242e84afc3fce07a63facc5b1
Igor Kramskoy <ig.kramskoy@it-justice.com>
2022-07-15 10:07:22 +0300

    refactor.

--- src/components/Pages/Transaction/Transaction.jsx
+++ src/components/Pages/Transaction/Transaction.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import {
+  Box,
+  Typography
+} from '@mui/material';
+
 export const Transaction = () => {
   return (
-    <div> Transaction </div>
+    <Box>
+      <Typography sx={{color: '#8C939D',textAlign:'left',}} variant="h4">Транзакции</Typography>
+    </Box>
   );
 }
