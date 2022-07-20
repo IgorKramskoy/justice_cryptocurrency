c407949f1bf8cc6b1709ee4c5892793e6d60bd1f
Igor Kramskoy <ig.kramskoy@it-justice.com>
2022-07-20 11:11:58 +0300

    refactor Wallet Pages.

--- src/components/Pages/Wallet/Wallet.styles.js
+++ src/components/Pages/Wallet/Wallet.styles.js
@@ -1,9 +1,23 @@
-import { Box, styled } from '@mui/material';
-import TableCell from '@mui/material/TableCell';
+import {
+  Box,
+  styled,
+  TableCell,
+  Toolbar,
+  Typography
+} from '@mui/material';
 
 export const BoxTableStyles  = styled(Box) (() => ({
-
-
+}))
+export const ToolbarStyles  = styled(Toolbar) (() => ({
+  display: 'flex',
+  justifyContent:'space-between',
+  borderBottom: '1px solid rgba(255, 255, 255, 0.1)',
+}))
+export const TypographyStyles  = styled(Typography) (() => ({
+  color: '#8C939D',
+  textAlign:'left',
+  marginTop:'30px',
+  fontSize:'15px'
 }))
 export const TableCellStyled = styled(TableCell) (() => ({
   '&': {
@@ -13,3 +27,5 @@ export const TableCellStyled = styled(TableCell) (() => ({
     borderBottom:'1px solid rgba(255, 255, 255, 0.1)'
   },
 }))
+
+
